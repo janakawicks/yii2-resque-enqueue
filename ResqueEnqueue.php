1be4da254399a23333ec991178b3aaf8784db6ba
Janaka Wickramasinghe <janaka@ascensionit.com.au>
2016-12-28 20:13:25 +0530

    Worker Listing Capability added

--- ResqueEnqueue.php
+++ ResqueEnqueue.php
@@ -36,4 +36,39 @@ class ResqueEnqueue extends Component
       );
   }
 
+  public function listWorkers($queues=[])
+  {
+    if(empty($queues))
+      return $this->_listWorkers($this->queue);
+    else {
+      $workers = [];
+      foreach($queues as $q){
+        $workers = $workers + $this->_listWorkers($q);
+      }
+      return $workers;
+    }
+  }
+
+  public function _listWorkers($queue)
+  {
+    //returns {host, pid, status, started_at || running details}
+    $workers = [];
+    $wrks = Yii::$app->redis->executeCommand('keys', ['resque:worker:*:'.$queue.'*']);
+    foreach($wrks as $w){
+      $arr = preg_split("/:/", $w);
+      if(!isset($arr[5])){
+        $workers[$arr[3]] = [ "host" => $arr[2], "pid" => $arr[3], "queue" => $arr[4], "status" => "running"];
+        $details = json_decode(Yii::$app->redis->executeCommand('get', [$w]), true);
+        $workers[$arr[3]]['started_at'] = date('Y-m-d h:i:s', strtotime($details["run_at"]));
+      }else if (!isset($workers[$arr[3]])){
+        $workers[$arr[3]] = [ "host" => $arr[2], "pid" => $arr[3], "queue" => $arr[4], "status" => "waiting" ];
+        $details = Yii::$app->redis->executeCommand('get', [$w]);
+        $workers[$arr[3]]['started_at'] = date('Y-m-d h:i:s', strtotime($details));
+      }
+    }
+
+    return $workers;
+  }
+
+
 }
